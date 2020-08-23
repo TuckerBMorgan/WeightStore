0cfcffc600ce7734accc2a6f634c0e53d56d2e71
marsmute <marsmute@protonmail.com>
2020-08-23 13:13:55 -0700

    FOrward prop for conv

--- src/main.rs
+++ src/main.rs
@@ -144,117 +144,46 @@ impl Conv2d {
         }
     }
 
-    fn break_up_image_by_channels_and_store(image_with_channels: &WeightToken, weight_store: &mut WeightStore) -> Vec<WeightToken> {
-        let full_image = weight_store.get_weights(&image_with_channels);
-
-        //The dimensions of the stored weights is, (channels, width, height)
-        let image_size = image_with_channels.dimensions[1] * image_with_channels.dimensions[2];
-        let chunks = full_image.chunks(image_size as usize);
-        let mut weight_tokens = vec![];
-        for c in chunks {
-            let as_vec = c.to_vec();
-            weight_tokens.push(weight_store.add_weights(as_vec, vec![image_with_channels.dimensions[1], image_with_channels.dimensions[2]]));
-        }
-        return weight_tokens;
-    }
-
-    fn single_filter_on_image(filter: &WeightToken, image_with_channel: &WeightToken, weight_store: &mut WeightStore) {
-        //This is a channels first library, got a problem with it, go write python
-        let channels = image_with_channel.dimensions[0];
-        let split_up_image = Conv2d::break_up_image_by_channels_and_store(&image_with_channel, weight_store);
-        let split_up_filter = Conv2d::break_up_image_by_channels_and_store(&filter, weight_store);
-
-        let mut convauled_channels = vec![];
-        for i in 0..channels {
-            let convaluted_image = Conv2d::single_image_single_channel_single_filter(&split_up_image[i], &split_up_filter[i], weight_store);
-            convauled_channels.push(convaluted_image);
-        }
-
-        let mut combined_convalted_image = vec![];
-        for i in 0..convauled_channels[0].len() {
-            let mut sum = 0.0;
-            for j in 0..channels {
-                sum += convauled_channels[j][i];
-            }
-            combined_convalted_image.push(sum);
-        }
-        let weight_token = weight_store.add_weights(combined_convalted_image, vec![1, image_with_channel.dimensions[1], image_with_channel.dimensions[2]]);
-    }
-
-    fn single_image_single_channel_single_filter(image: &WeightToken, filter: &WeightToken, weight_store: &mut WeightStore) -> Vec<f32> {
-        let image_values = weight_store.get_weights(image);
-        let filter_values = weight_store.get_weights(filter);
-
-        let image_values_dimensions = (image.dimensions[0], image.dimensions[1]);
-        let input_weight_matrix = Array::from_shape_vec(image_values_dimensions, image_values).unwrap();
-
-        let filter_values_dimensions = (filter.dimensions[0], filter.dimensions[1]);
-        let filter_weight_matrix = Array::from_shape_vec(filter_values_dimensions, filter_values).unwrap();
-
-        let mut results = vec![];
-        for f in filter_weight_matrix.windows(image_values_dimensions) {
-            let result = f.dot(&input_weight_matrix);
-            results.push(result);
-        }
-        
-        let mut final_result = vec![];
-        for matrix in results {
-            final_result.extend(matrix.iter().map(|x|*x));
-        }
-        return final_result;
-    }
-
     //this will remap the image into a array such that it can be dot producted by a similar 
     //remapped matrix of the filters to preform the conv operation
-    fn remap_image_to_dense(image: &WeightToken, filter_dimensions: &Vec<usize>, weight_store: &mut WeightStore) -> WeightToken {
+    fn remap_image_to_dot_product_matrix(image: &WeightToken, filter_dimensions: &Vec<usize>, weight_store: &mut WeightStore) -> WeightToken {
         
         let kernal_size = filter_dimensions[2];
         let image_array = weight_store.get_weights(&image);
         let (height, width) = (image.dimensions[1], image.dimensions[2]);
+        let number_of_channels = image.dimensions[0];
         
-        //This will create sub vectors of image channel
-        let channels = image_array.chunks(height * width);
-        let mut full_image_with_channels = vec![];
         let number_of_kernals_per_single_channel_of_image = (height - kernal_size) + 1;
-        for sub_image in channels {
-            let mut single_channel = vec![];
-            //I hate doing this, but I also don't want to figure out the fucking array index math
-            //dont look at the next function where I do actually work on the array index math
-            let image_as_matrix = Array::from_shape_vec((height, width), sub_image.to_vec()).unwrap();
-            for w in image_as_matrix.windows((kernal_size, kernal_size)) {
-                let s : Vec<f32> = w.iter().map(|x|*x).collect();
-                single_channel.extend(s);
-            }
-            full_image_with_channels.extend(single_channel);
+        let image_as_matrix = Array::from_shape_vec((number_of_channels, height, width), image_array.to_vec()).unwrap();
+        let windows = image_as_matrix.windows((number_of_channels,kernal_size, kernal_size));
+        let mut remapped_image = vec![];
+        for w in windows {
+            remapped_image.extend(w.iter());
         }
-        
-        return weight_store.add_weights(full_image_with_channels, vec![image.dimensions[0], number_of_kernals_per_single_channel_of_image, kernal_size * kernal_size]);
+        return weight_store.add_weights(remapped_image, vec![number_of_kernals_per_single_channel_of_image, kernal_size * kernal_size * image.dimensions[0],]);
     }
 
-    fn remap_filters_into_dot_product_matrix(filters: &WeightToken, image_dimensions: &Vec<usize>, weight_store: &mut WeightStore) -> WeightToken {
+    fn remap_filters_into_dot_product_matrix(filters: &WeightToken, weight_store: &mut WeightStore) -> WeightToken {
         //Filter dimensions should be(number of filters, channels, height, weight)
-        //Image dimensions hsould be (channels, height, weight)
-        let number_of_channels = image_dimensions[0];
+        let number_of_channels = filters.dimensions[1];
         let filter_size = filters.dimensions[2];
         let number_of_filters = filters.dimensions[0];
-        let (image_height, _) = (image_dimensions[1], image_dimensions[2]);
-        let number_of_kernals_per_single_channel_of_image = (image_height - filter_size) + 1;
         //need to copy a single channel of a filter number_filters_per_single_of_image times, flatten it, then copy that for each channel, for each filter
         let combined_height_width = filters.dimensions[2] * filters.dimensions[3];
         let filter_weights : Vec<f32> = weight_store.get_weights(&filters);
         //this could be paralized without MUCH problem
         let mut stacked_filter_weights = vec![];
         for i in 0..number_of_filters {
+            let mut single_filter_all_channels : Vec<f32> = vec![];
             for j in 0..number_of_channels { 
                 let start_index = (j * filter_size * filter_size) + (i * number_of_channels * filter_size * filter_size);
                 let filter_weights = &filter_weights[start_index..(filter_size * filter_size)];
-                for k in 0..number_of_kernals_per_single_channel_of_image {
-                    stacked_filter_weights.extend(filter_weights.clone());
-                }
+                single_filter_all_channels.extend(filter_weights.clone());
             }
+            stacked_filter_weights.extend(single_filter_all_channels);
         }
-        let len = stacked_filter_weights.len();
-        return weight_store.add_weights(stacked_filter_weights, vec![number_of_filters, number_of_channels, number_of_kernals_per_single_channel_of_image ,combined_height_width]);
+
+        return weight_store.add_weights(stacked_filter_weights, vec![number_of_filters, combined_height_width * number_of_channels]);
     }
     
 }
@@ -267,36 +196,21 @@ impl Node for Conv2d {
         //the weights for the filters, they are form, number of filters, channels, height, weight
         let filter_weights_token = weight_tokens[0];
         let input_weight_token = weight_tokens[1];
-        let filter_weight_array = weight_store.get_weights(&filter_weights_token);
-        let remapped_filters = Conv2d::remap_filters_into_dot_product_matrix(filter_weights_token, &input_weight_token.dimensions, weight_store);
-        let remapped_image = Conv2d::remap_image_to_dense(&input_weight_token, &filter_weights_token.dimensions, weight_store);
+        let remapped_filters = Conv2d::remap_filters_into_dot_product_matrix(filter_weights_token, weight_store);
+        let remapped_image = Conv2d::remap_image_to_dot_product_matrix(&input_weight_token, &filter_weights_token.dimensions, weight_store);
         
         let remapped_filters_weights = weight_store.get_weights(&remapped_filters);
         let remapped_image_weights = weight_store.get_weights(&remapped_image);
 
         //This is not quite correct, not taking into account Channel;s
-        let remapped_filters_matrix = Array::from_shape_vec((remapped_filters.dimensions[0], remapped_filters.dimensions[1], remapped_filters.dimensions[2], remapped_filters.dimensions[3]), remapped_filters_weights).unwrap();
-        let remapped_image_matrix = Array::from_shape_vec((remapped_image.dimensions[0], remapped_image.dimensions[1], remapped_image.dimensions[2]), remapped_image_weights).unwrap();
+        let remapped_filters_matrix = Array::from_shape_vec((remapped_filters.dimensions[0], remapped_filters.dimensions[1]), remapped_filters_weights).unwrap();
+        let remapped_image_matrix = Array::from_shape_vec((remapped_image.dimensions[0], remapped_image.dimensions[1]), remapped_image_weights).unwrap();
         //For each filter
-        for f in remapped_filters_matrix.outer_iter() {
-            //For each channel
-            let mut channels = vec![];
-            let mut kernal_channel = f.outer_iter();
-            let mut image_channel = remapped_image_matrix.outer_iter();
-            let combined = kernal_channel.zip(image_channel);
-            for (kc, im) in combined {
-                let result = im.dot(&kc);
-                channels.push(combined);
-            }
-            //flatten them
-            //sum them
-            //Store them
-            //drink yourself to death
-            
-        }
-
-
-        return vec![];
+        let result = remapped_image_matrix.dot(&remapped_filters_matrix);
+        let stored_resulted : Vec<f32> = result.iter().map(|x|*x).collect();
+        let number_of_kernals_per_single_channel_of_image = (input_weight_token.dimensions[1] - filter_weights_token.dimensions[2]) + 1;
+        //TODO: I may need to shuffle stored_resulted so that each of the outputs of an image are stacked ontop of each other
+        return vec![weight_store.add_weights(stored_resulted, vec![filter_weights_token.dimensions[0], number_of_kernals_per_single_channel_of_image, number_of_kernals_per_single_channel_of_image])];
     }
 
 
